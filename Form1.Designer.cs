295acb40fccb8ca19c474e524219535ce273a156
Nolan Pawelski <32725534+pawnol@users.noreply.github.com>
2021-12-14 10:45:04 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace Coins
+{
+    partial class CoinsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.penniesTextBox = new System.Windows.Forms.TextBox();
+            this.nickelsTextBox = new System.Windows.Forms.TextBox();
+            this.dimesTextBox = new System.Windows.Forms.TextBox();
+            this.quartersTextBox = new System.Windows.Forms.TextBox();
+            this.calculateTotalButton = new System.Windows.Forms.Button();
+            this.moneyListBox = new System.Windows.Forms.ListBox();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pennies:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(85, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nickels:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(93, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Dimes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 146);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Quarters:";
+            // 
+            // penniesTextBox
+            // 
+            this.penniesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.penniesTextBox.Location = new System.Drawing.Point(167, 38);
+            this.penniesTextBox.Name = "penniesTextBox";
+            this.penniesTextBox.Size = new System.Drawing.Size(100, 29);
+            this.penniesTextBox.TabIndex = 4;
+            // 
+            // nickelsTextBox
+            // 
+            this.nickelsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nickelsTextBox.Location = new System.Drawing.Point(167, 73);
+            this.nickelsTextBox.Name = "nickelsTextBox";
+            this.nickelsTextBox.Size = new System.Drawing.Size(100, 29);
+            this.nickelsTextBox.TabIndex = 5;
+            // 
+            // dimesTextBox
+            // 
+            this.dimesTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dimesTextBox.Location = new System.Drawing.Point(167, 108);
+            this.dimesTextBox.Name = "dimesTextBox";
+            this.dimesTextBox.Size = new System.Drawing.Size(100, 29);
+            this.dimesTextBox.TabIndex = 6;
+            // 
+            // quartersTextBox
+            // 
+            this.quartersTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quartersTextBox.Location = new System.Drawing.Point(167, 143);
+            this.quartersTextBox.Name = "quartersTextBox";
+            this.quartersTextBox.Size = new System.Drawing.Size(100, 29);
+            this.quartersTextBox.TabIndex = 7;
+            // 
+            // calculateTotalButton
+            // 
+            this.calculateTotalButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateTotalButton.Location = new System.Drawing.Point(67, 178);
+            this.calculateTotalButton.Name = "calculateTotalButton";
+            this.calculateTotalButton.Size = new System.Drawing.Size(200, 64);
+            this.calculateTotalButton.TabIndex = 8;
+            this.calculateTotalButton.Text = "Calculate Total Money";
+            this.calculateTotalButton.UseVisualStyleBackColor = true;
+            // 
+            // moneyListBox
+            // 
+            this.moneyListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.moneyListBox.FormattingEnabled = true;
+            this.moneyListBox.ItemHeight = 24;
+            this.moneyListBox.Location = new System.Drawing.Point(67, 248);
+            this.moneyListBox.Name = "moneyListBox";
+            this.moneyListBox.Size = new System.Drawing.Size(200, 172);
+            this.moneyListBox.TabIndex = 9;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(89, 426);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 46);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(170, 426);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 46);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // CoinsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 511);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.moneyListBox);
+            this.Controls.Add(this.calculateTotalButton);
+            this.Controls.Add(this.quartersTextBox);
+            this.Controls.Add(this.dimesTextBox);
+            this.Controls.Add(this.nickelsTextBox);
+            this.Controls.Add(this.penniesTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "CoinsForm";
+            this.Text = "Coins";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox penniesTextBox;
+        private System.Windows.Forms.TextBox nickelsTextBox;
+        private System.Windows.Forms.TextBox dimesTextBox;
+        private System.Windows.Forms.TextBox quartersTextBox;
+        private System.Windows.Forms.Button calculateTotalButton;
+        private System.Windows.Forms.ListBox moneyListBox;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Coins
+ * Pawelski
+ * 12/14/2021
+ * This program should calculate the total money a person
+ * has when they enter the number of pennies, nickels, dimes,
+ * and quarters, but the program is missing code. Add the necessary 
+ * code to finish the program.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coins
+{
+    public partial class CoinsForm : Form
+    {
+        public CoinsForm()
+        {
+            InitializeComponent();
+        }
+
+        /*
+         * This will close the application when the "Exit" button
+         * is clicked.
+         */
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /*
+         * This will clear the text boxes and list box.
+         */
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            penniesTextBox.Text = "";
+            nickelsTextBox.Text = "";
+            dimesTextBox.Text = "";
+            quartersTextBox.Text = "";
+            moneyListBox.Items.Clear();
+        }
+    }
+}
